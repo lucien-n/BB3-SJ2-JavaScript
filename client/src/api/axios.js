b9357cda527d8f8092493e81069119a7a58a35f9
Lucien <contact@lucienn.dev>
2026-03-04 10:39:58 +0100

    use backend url in api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
+import { BACKEND_URL } from "../constants";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5030/api',
+  baseURL: `${BACKEND_URL}/api`,
 });
 
 // Add a request interceptor to attach JWT token
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
