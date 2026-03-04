90caa8e1ae058f984f9e741acee838a37ea914cc
Lucien <contact@lucienn.dev>
2026-03-04 11:42:03 +0100

    fix: validator export

--- server/validators/auth.validator.js
+++ server/validators/auth.validator.js
@@ -1,15 +1,15 @@
-import z from "zod";
+const z = require("zod").default;
 
 const USERNAME_MIN_LENGTH = 3;
 const PASSWORD_MIN_LENGTH = 6;
 
-export const registerSchema = z.object({
+exports.registerSchema = z.object({
   username: z.string().min(USERNAME_MIN_LENGTH),
   email: z.email(),
   password: z.string().min(PASSWORD_MIN_LENGTH),
 });
 
-export const loginSchema = z.object({
+exports.loginSchema = z.object({
   email: z.email(),
   password: z.string().min(PASSWORD_MIN_LENGTH),
 });
